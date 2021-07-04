69b606a72e9daa2dc70e19a1d37594ece167a53d
sunkai <286190665@qq.com>
2021-07-04 16:35:47 +0800

    mac-分支提交代码

---
+++ springcloud-agile/src/main/java/cn/com/sk/springcloudagile/SpringcloudAgileApplication.java
@@ -0,0 +1,13 @@
+package cn.com.sk.springcloudagile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudAgileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudAgileApplication.class, args);
+	}
+
+}
